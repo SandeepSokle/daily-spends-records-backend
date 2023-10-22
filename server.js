bd9c9e097b23b7be78a6526d2f3c632a1714d8b3
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-10-22 12:54:37 +0530

    update

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const argv = require("yargs").argv;
 const { mongoConnection } = require("./mongo/config");
 const cors = require("cors");
+const https = require("https")
+const fs = require("fs")
+
 const rootRouter = require("./src/routers/rootRouter");
 const app = express();
 
@@ -16,6 +19,21 @@ app.get("/",()=>{
   res.status(200).send("Hi User, Welcome")
 })
 app.use("/api/2023", rootRouter);
+
+const base = "/etc/letsencrypt/live/moneyserver.webtechbharat.com/"
+const PEM = `${base}/privkey.pem`
+const CERT = `${base}/fullchain.pem`
+const CHAIN = `${base}/chain.pem`
+
+const options = {
+  key: fs.readFileSync(PEM),
+  cert: fs.readFileSync(CERT),
+  ca: fs.readFileSync(CHAIN),
+};
+
+https.createServer(options, app).listen(443, () => {
+  console.log("Listening securely on", 443);
+});
 
 mongoConnection();
 
